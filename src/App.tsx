28955aa360d48539ec99dd4aa44c9256db108a88
Cesar Perales <c.casurpie@gmail.com>
2026-08-25 10:50:48 -0500

    Configure Vite and GitHub Actions for GitHub Pages deployment

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { Sidebar } from './components/layout/Sidebar';
 import { DashboardView } from './views/DashboardView';
 import { GoalsView } from './views/GoalsView';
